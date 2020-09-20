ed7af367eead065ecb2e95df43e66d79dbce0fce
gaidukk <71583987+gaidukk@users.noreply.github.com>
2020-09-20 15:21:18 +0700

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using ExcelObj = Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Cars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenTable(dataGridView1);
+        }
+
+        public static void OpenTable(DataGridView TableInf)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                DefaultExt = "*.xls;*.xlsx",
+                Filter = "Excel Sheet(*.xlsx)|*.xlsx",
+                Title = "Выберите документ для загрузки данных"
+            };
+            ExcelObj.Application app = new ExcelObj.Application();
+            ExcelObj.Workbook workbook;
+            ExcelObj.Worksheet NwSheet;
+            ExcelObj.Range ShtRange;
+            DataTable dt = new DataTable();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                workbook = app.Workbooks.Open(ofd.FileName, Missing.Value,
+                Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+                Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+                Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+                Missing.Value);
+
+                NwSheet = (ExcelObj.Worksheet)workbook.Sheets.get_Item(1);
+                ShtRange = NwSheet.UsedRange;
+                for (int Cnum = 1; Cnum <= ShtRange.Columns.Count; Cnum++)
+                {
+                    dt.Columns.Add(new DataColumn((ShtRange.Cells[1, Cnum] as ExcelObj.Range).Value2.ToString()));
+                }
+                dt.AcceptChanges();
+
+                string[] columnNames = new String[dt.Columns.Count];
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    columnNames[0] = dt.Columns[i].ColumnName;
+                }
+
+                for (int Rnum = 2; Rnum <= ShtRange.Rows.Count; Rnum++)
+                {
+                    DataRow dr = dt.NewRow();
+                    for (int Cnum = 1; Cnum <= ShtRange.Columns.Count; Cnum++)
+                    {
+                        if ((ShtRange.Cells[Rnum, Cnum] as ExcelObj.Range).Value2 != null)
+                        {
+                            dr[Cnum - 1] =
+                (ShtRange.Cells[Rnum, Cnum] as ExcelObj.Range).Value.ToString();
+                        }
+                    }
+                    dt.Rows.Add(dr);
+                    dt.AcceptChanges();
+                }
+
+                TableInf.DataSource = dt;
+                app.Quit();
+            }
+            else
+                Application.Exit();
+            app.Quit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveTable(dataGridView1);
+            MessageBox.Show("Сохранено");
+        }
+
+        void SaveTable(DataGridView Table)
+        {
+            string path = System.IO.Directory.GetCurrentDirectory() + @"\" + "Cars.xlsx";
+
+            Excel.Application excelapp = new Excel.Application();
+            Excel.Workbook workbook = excelapp.Workbooks.Add();
+            Excel.Worksheet worksheet = workbook.ActiveSheet;
+
+            for (int i = 1; i < dataGridView1.RowCount + 1; i++)
+            {
+                for (int j = 1; j < dataGridView1.ColumnCount + 1; j++)
+                {
+                    worksheet.Rows[i].Columns[j] = dataGridView1.Rows[i - 1].Cells[j - 1].Value;
+                }
+            }
+            excelapp.AlertBeforeOverwriting = false;
+            workbook.SaveAs(path);
+            excelapp.Quit();
+
+        }
+    }
+}
